0489b3a6b24c1cb6e1bc570237ab759f350ac903
JaviCraftero <55707374+JaviCraftero@users.noreply.github.com>
2019-10-18 12:51:36 -0500

    Registro++
    Se termino el diseño del formulario y se agrego la funcin de un calendario :D :+1:

--- app/src/main/java/mx/ipn/e_fortlessshoping/SignUp.java
+++ app/src/main/java/mx/ipn/e_fortlessshoping/SignUp.java
@@ -2,13 +2,48 @@ package mx.ipn.e_fortlessshoping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+import android.app.Dialog;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
 
+    EditText fecha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        fecha = (EditText) findViewById(R.id.FecNac);
+        fecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.FecNac:
+                        showDatePickerDialog();
+                        break;
+                }
+            }
+        });
+    }
+
+    private String twoDigits(int n) {
+        return (n<=9) ? ("0"+n) : String.valueOf(n);
+    }
+
+    private void showDatePickerDialog(){
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // +1 because January is zero
+                final String selectedDate = twoDigits(day) + " / " + twoDigits(month+1) + " / " + year;
+                fecha.setText(selectedDate);
+            }
+        });
+        newFragment.show(getSupportFragmentManager(),"datePicker");
     }
 }
