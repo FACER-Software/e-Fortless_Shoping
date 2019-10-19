d3346f4fe7091594f4685ff1bc0c6cef5b4960b8
JaviCraftero <jehb2002@hotmail.com>
2019-10-19 13:43:09 -0500

    Calendario
    Se arreglo y termino la funcion del calendario c:

--- app/src/main/java/mx/ipn/e_fortlessshoping/DatePickerFragment.java
+++ app/src/main/java/mx/ipn/e_fortlessshoping/DatePickerFragment.java
@@ -10,8 +10,8 @@ import androidx.fragment.app.DialogFragment;
 
 import java.util.Calendar;
 
-public class DatePickerFragment extends DialogFragment
-        implements DatePickerDialog.OnDateSetListener {
+public class DatePickerFragment extends DialogFragment {
+
     private DatePickerDialog.OnDateSetListener listener;
 
     public static DatePickerFragment newInstance(DatePickerDialog.OnDateSetListener listener) {
@@ -27,22 +27,12 @@ public class DatePickerFragment extends DialogFragment
     @Override
     @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current date as the default date in the picker
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
 
-        // Selected date (initial value)
-
-
-
-        // Create a new instance of DatePickerDialog and return it
-        return new DatePickerDialog(getActivity(), this, year - 18, month, day);
-    }
-
-    public void onDateSet(DatePicker view, int year, int month, int day) {
-        // Do something with the date chosen by the user
+        return new DatePickerDialog(getActivity(), listener, year - 18, month, day);
     }
 
 }
--- app/src/main/java/mx/ipn/e_fortlessshoping/SignUp.java
+++ app/src/main/java/mx/ipn/e_fortlessshoping/SignUp.java
@@ -42,13 +42,13 @@ public class SignUp extends AppCompatActivity {
             }
         });
 
-        fecha = (EditText) findViewById(R.id.FecNac);
+        fecha = findViewById(R.id.FecNac);
         fecha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 switch (view.getId()){
                     case R.id.FecNac:
-                        showDatePickerDialog();
+                        showDatePickerDialog(fecha);
                         break;
                 }
             }
@@ -59,13 +59,13 @@ public class SignUp extends AppCompatActivity {
         return (n<=9) ? ("0"+n) : String.valueOf(n);
     }
 
-    private void showDatePickerDialog(){
+    private void showDatePickerDialog(EditText fecha){
         DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                 // +1 because January is zero
                 final String selectedDate = twoDigits(day) + " / " + twoDigits(month+1) + " / " + year;
-                fecha.setText(selectedDate);
+                SignUp.this.fecha.setText(selectedDate);
             }
         });
         newFragment.show(getSupportFragmentManager(),"datePicker");
